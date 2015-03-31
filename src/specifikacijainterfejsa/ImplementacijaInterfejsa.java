770ff60226d9547cc6093df73067c645bd491882
ivanazacevic <ivana.zecevic94@yahoo.com>
2015-03-31 23:49:18 +0200

    Implementirana metoda dodajKurs na master grani.

--- src/specifikacijainterfejsa/ImplementacijaInterfejsa.java
+++ src/specifikacijainterfejsa/ImplementacijaInterfejsa.java
@@ -4,22 +4,28 @@ import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
 import menjacnica.Kurs;
+import menjacnica.Valuta;
 
 public class ImplementacijaInterfejsa implements Interfejs {
-
-	public void dodajKurs(GregorianCalendar datum, String naziv,
+	
+	Valuta[] valuteNiz = new Valuta[100];
+	
+    public void dodajKurs(GregorianCalendar datum, String naziv,
 			LinkedList<Kurs> kurs) {
-		// TODO Auto-generated method stub
+		for(int i=0;i<valuteNiz.length;i++)
+			if(valuteNiz[i].getNaziv().equals(naziv)){
+				valuteNiz[i].setDatum(datum);
+				valuteNiz[i].setKursnaLista(kurs);
+			}
 
 	}
 
 	public void obrisiKurs(GregorianCalendar datum, String naziv) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
-	public Kurs pronadjiKurs(GregorianCalendar datum, String naziv) {
-		// TODO Auto-generated method stub
+	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) {
+		
 		return null;
 	}
 
--- src/specifikacijainterfejsa/Interfejs.java
+++ src/specifikacijainterfejsa/Interfejs.java
@@ -4,11 +4,12 @@ import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
 import menjacnica.Kurs;
+import menjacnica.Valuta;
 
 public interface Interfejs {
 
 	public void dodajKurs(GregorianCalendar datum,String naziv,LinkedList<Kurs> kurs);
 	public void obrisiKurs(GregorianCalendar datum, String naziv);
-	public Kurs pronadjiKurs(GregorianCalendar datum, String naziv);
+	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv);
 	
 }
