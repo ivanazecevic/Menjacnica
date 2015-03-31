249d484938db88399f4e95cabbfb4797a90c6c95
ivanazacevic <ivana.zecevic94@yahoo.com>
2015-03-31 23:30:09 +0200

    Napravljena je nova klasa ImplementacijaInterfejsa.

---
+++ src/specifikacijainterfejsa/ImplementacijaInterfejsa.java
@@ -0,0 +1,26 @@
+package specifikacijainterfejsa;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import menjacnica.Kurs;
+
+public class ImplementacijaInterfejsa implements Interfejs {
+
+	public void dodajKurs(GregorianCalendar datum, String naziv,
+			LinkedList<Kurs> kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void obrisiKurs(GregorianCalendar datum, String naziv) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Kurs pronadjiKurs(GregorianCalendar datum, String naziv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
