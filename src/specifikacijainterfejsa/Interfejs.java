9514cac20fe35862896476b3d00dc339690ce3a2
ivanazacevic <ivana.zecevic94@yahoo.com>
2015-03-31 23:28:32 +0200

    Ispravljen ulazni parametar u metodi dodajKurs.

--- src/specifikacijainterfejsa/Interfejs.java
+++ src/specifikacijainterfejsa/Interfejs.java
@@ -1,12 +1,13 @@
 package specifikacijainterfejsa;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.Kurs;
 
 public interface Interfejs {
 
-	public void dodajKurs(GregorianCalendar datum,String naziv,Kurs kurs);
+	public void dodajKurs(GregorianCalendar datum,String naziv,LinkedList<Kurs> kurs);
 	public void obrisiKurs(GregorianCalendar datum, String naziv);
 	public Kurs pronadjiKurs(GregorianCalendar datum, String naziv);
 	
