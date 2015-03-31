bed1178d098f747864ecb7bf990b49feaa8f5d6d
ivanazacevic <ivana.zecevic94@yahoo.com>
2015-03-31 23:41:38 +0200

    Potpuno implementirane metode u klasi ImplementacijaInterfejsa.

--- src/specifikacijainterfejsa/ImplementacijaInterfejsa.java
+++ src/specifikacijainterfejsa/ImplementacijaInterfejsa.java
@@ -4,22 +4,32 @@ import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
 import menjacnica.Kurs;
+import menjacnica.Valuta;
 
 public class ImplementacijaInterfejsa implements Interfejs {
+	
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	public void dodajKurs(GregorianCalendar datum, String naziv,
 			LinkedList<Kurs> kurs) {
-		// TODO Auto-generated method stub
+		for(int i=0;i<valute.size();i++)
+			if(valute.get(i).getNaziv().equals(naziv)){
+				valute.get(i).setKursnaLista(kurs);
+				valute.get(i).setDatum(datum);
+			}
 
 	}
 
 	public void obrisiKurs(GregorianCalendar datum, String naziv) {
-		// TODO Auto-generated method stub
-
+		for(int i=0;i<valute.size();i++)
+			if(valute.get(i).getDatum().equals(datum)&& valute.get(i).getNaziv().equals(naziv))
+               valute.remove(valute.get(i).getKursnaLista());
 	}
 
-	public Kurs pronadjiKurs(GregorianCalendar datum, String naziv) {
-		// TODO Auto-generated method stub
+	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) {
+		for(int i=0;i<valute.size();i++)
+			if(valute.get(i).getDatum().equals(datum)&& valute.get(i).getNaziv().equals(naziv))
+				return valute.get(i);
 		return null;
 	}
 
